2a2821cf86f0ed46dbdce3784331f5d549d6161f
Evgeniy <zhe626221@yandex.ru>
2024-07-08 15:07:28 +0300

    masks.py корректировка

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """Функция маскировки номера карты"""
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]}{card_number[4:6]}{'*' * 2}{'*' * 4}{card_number[12:]}"
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
     else:
         return None
 
