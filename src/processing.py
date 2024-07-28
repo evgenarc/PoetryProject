1ab0b5cfa86d946f88ee127d61d24f2a7a729e20
Evgeniy <zhe626221@yandex.ru>
2024-07-28 15:48:47 +0300

    В модуль processing.py добавлена функция sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -4,3 +4,6 @@ def filter_by_state(data, state="EXECUTED"):
     return [item for item in data if item.get("state") == state]
 
 
+def sort_by_date(data, reverse=True):
+    """Функция принимающая список словарей и параметр, задающий порядок сортировки (по умолчанию — убывание)"""
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
